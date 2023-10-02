68b561ec878c25ee24f85f35cf7d24fd82932bb6
Nikita <overclocker96@gmail.com>
2023-10-02 10:57:35 +0500

    Маленькие поправки

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ const isEqualOrLess = (string, length) => string.length<=length;
 const isPalindromic = (string) =>{
 
   let preparedString = string.toLowerCase().replaceAll(' ', '');
-  let reversedString = preparedString.split("").reverse().join("");
+  let reversedString = preparedString.split('').reverse().join('');
 
   return reversedString===preparedString;
 };
