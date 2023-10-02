f1b07548eb7305c8e209c250da1ce43598df8a14
Nikita <overclocker96@gmail.com>
2023-10-02 10:48:00 +0500

    Доработка метода

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,8 @@ const isEqualOrLess = (string, length) => string.length<=length;
 
 const isPalindromic = (string) =>{
 
-  let preparedString = string.toLowerCase().replaceAll(" ", "");
-  let reversedString = "";
-
-  for (let i = preparedString.length-1; i>=0;  i--){
-      reversedString += preparedString[i];
-  }
+  let preparedString = string.toLowerCase().replaceAll(' ', '');
+  let reversedString = preparedString.split("").reverse().join("");
 
   return reversedString===preparedString;
-}
+};
