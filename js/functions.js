4b40859affe01ba65d209f58833be85327ef21f4
Nikita <overclocker96@gmail.com>
2023-10-02 11:10:48 +0500

    Заменил let на const

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,8 @@ const isEqualOrLess = (string, length) => string.length<=length;
 
 const isPalindromic = (string) =>{
 
-  let preparedString = string.toLowerCase().replaceAll(' ', '');
-  let reversedString = preparedString.split('').reverse().join('');
+  const preparedString = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = preparedString.split('').reverse().join('');
 
   return reversedString===preparedString;
 };
